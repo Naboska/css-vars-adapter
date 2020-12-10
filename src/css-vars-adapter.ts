a1f972c04e796cb50aa3065d332467670c214abf
Nabos <Nabos.dezigner@gmail.com>
2020-12-10 22:35:16 +0300

    feat: add in getVariables global variables, setVariables without replace

--- src/css-vars-adapter.ts
+++ src/css-vars-adapter.ts
@@ -12,7 +12,9 @@ const CSS_VAR_PREFIX = '--';
 const MAIN_ELEMENT = document.documentElement;
 
 const formatVar = (key: string): string => key.startsWith(CSS_VAR_PREFIX) ? key : `${CSS_VAR_PREFIX}${key}`;
+const getVariable = (key: string) => getComputedStyle(MAIN_ELEMENT).getPropertyValue(key);
 const setVariable = (key: string, variable: string): void => MAIN_ELEMENT.style.setProperty(key, variable);
+
 const runTry = <T>(fn: (args: T) => any) => (args: T) => {
   try {
     return fn(args)
@@ -20,23 +22,31 @@ const runTry = <T>(fn: (args: T) => any) => (args: T) => {
   }
 }
 
+const getStyleVariable = (style: CSSStyleDeclaration, vars = {}) => {
+  Array.from(style).forEach((name: string) => {
+    if (name && name.startsWith(CSS_VAR_PREFIX) && !vars[name])
+      vars[name] = getVariable(name)
+  });
+
+  return vars;
+};
+
 export const getVariables = () => {
   const styleSheets: TStyleSheet[] = Array.from(document.styleSheets)
     .map(runTry(e => e.cssRules))
     .filter(Boolean);
 
-  return styleSheets.reduce((vars, cssRules) => {
-    if (cssRules.length) Array.from(cssRules as any)
-      .forEach(({style}) => {
-        if (style) Array.from(style)
-          .forEach((name: string) => {
-            if (name && name.startsWith('--') && !vars[name])
-              vars[name] = getComputedStyle(MAIN_ELEMENT).getPropertyValue(name)
-          })
-      })
+  let vars = getStyleVariable(MAIN_ELEMENT.style, {});
+
+  styleSheets.forEach(cssRules => {
+    if (cssRules.length) Array.from(cssRules as any).forEach(({style}) => {
+      if (style) getStyleVariable(style, vars);
+    })
 
     return vars;
-  }, {})
+  });
+
+  return vars;
 }
 
 export const setVariables = <T extends TVariables = {}>(values: T, options: TOptions = {}): void => {
@@ -45,6 +55,10 @@ export const setVariables = <T extends TVariables = {}>(values: T, options: TOpt
   const val = Object.entries(values);
   const variables: [string, string][] = val.map(([key, value]) => [formatVar(key), String(value)]);
 
-  if (isReplace) variables.forEach(style => setVariable(...style));
-
+  variables.forEach(style => {
+    if (isReplace) setVariable(...style)
+    else {
+      if (!getVariable(style[0])) setVariable(...style)
+    }
+  });
 }
